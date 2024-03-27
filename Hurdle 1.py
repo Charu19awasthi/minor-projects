7a824add5aa69a6dc13501eda07b2dc3ff742b40
Charu19awasthi <148922861+Charu19awasthi@users.noreply.github.com>
2024-03-27 23:41:39 +0530

    Add files via upload

---
+++ Hurdle 1.py
@@ -0,0 +1,24 @@
+def turnright():
+    turn_left()
+    turn_left()
+    turn_left()
+n=6
+def repeat():
+    move()
+    turn_left()
+    move()
+    turnright()
+    move()
+    turnright()
+    move()
+    turn_left()
+while n>=1:
+    repeat()
+    n=n-1
+
+    
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
